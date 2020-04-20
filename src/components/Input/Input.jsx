957448bb41974c353d4f38b0fa8ccd83254bd135
Andrew2432 <drew.devops@gmail.com>
2020-04-20 19:26:53 +0530

    Change style of Input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,10 +2,27 @@ import React from "react";
 
 function Input() {
 	return (
-		<form>
-			<input type="text" name="todoItem" placeholder="Example: Buy water" />
-			<button type="submit">Add</button>
-		</form>
+		<div className="row">
+			<div classname={`col s12`}>
+				<form>
+					<div className={`row`}>
+						<div className={`col s8`}>
+							<input
+								type="text"
+								name="todoItem"
+								placeholder="Example: Buy water"
+							/>
+							<label htmlFor="todoItem">Todo:</label>
+						</div>
+						<div className={`col s4`}>
+							<button className={`btn btn-small`} type="submit">
+								Add
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
 	);
 }
 
