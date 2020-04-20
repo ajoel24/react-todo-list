a795e54d5ed91be40af18f6430e4dbbb07ba5ae7
Andrew2432 <drew.devops@gmail.com>
2020-04-20 18:51:29 +0530

    Add code to Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+	return (
+		<header>
+			<nav>
+				<div class="nav-wrapper">
+					<a href="#" class="brand-logo center">
+						TodoList
+					</a>
+				</div>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
