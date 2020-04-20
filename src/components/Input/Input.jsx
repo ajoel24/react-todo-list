1303f7f08c74a1eebe2aa4351cacaec925dd8ff5
Andrew2432 <drew.devops@gmail.com>
2020-04-20 19:48:37 +0530

    Add Button to Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import InputField from "../InputField/InputField";
+import Button from "../Button/Button";
 
 function Input() {
 	return (
@@ -15,12 +16,7 @@ function Input() {
 							/>
 						</div>
 						<div className={`col s4 center`}>
-							<button
-								className={`btn btn-medium white blue-text`}
-								type="submit"
-							>
-								<b>Add</b>
-							</button>
+							<Button type="submit" name="submit" innerHTML="Add" />
 						</div>
 					</div>
 				</form>
