ef3f9e018f23d2ad002288cb9bdd051fefc8213e
Andrew2432 <drew.devops@gmail.com>
2020-04-20 19:10:51 +0530

    Add code to TodoContainer component

---
+++ src/components/TodoContainer/TodoContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TodoContainer() {
+	return (
+		<main>
+			<div className={`container`}>
+				<div className={`card-panel blue white-text`}>
+					<h1>Hello</h1>
+				</div>
+			</div>
+		</main>
+	);
+}
+
+export default TodoContainer;
